4b9a12e3d3058533ed3e9501c9cd92d26b0284c1
Sadman Sakib <32223045+SakibNeloy@users.noreply.github.com>
2018-05-02 14:18:47 +0600

    web scraping code

---
+++ quote-scrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "http://quotes.toscrape.com"
+url = "/page/1"
+
+while url:
+    res = requests.get(f"{base_url}{url}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    quotes = soup.find_all(class_="quote")
+
+    for quote in quotes:
+        text = quote.find(class_="text").get_text()
+        print(text)
+        author = quote.find(class_="author").get_text()
+        print(author)
+       
+    next_btn = soup.find(class_="next")
+    url = next_btn.find("a") ["href"] if next_btn else None
